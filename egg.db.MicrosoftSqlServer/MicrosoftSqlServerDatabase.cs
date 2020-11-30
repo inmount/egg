13d4fc14e42209aac2d98eef6b98d2683247ef19
inmount <inmount@live.cn>
2020-11-30 20:17:54 +0800

    创建数据库模块

---
+++ egg.db.MicrosoftSqlServer/MicrosoftSqlServerDatabase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace egg.db {
+
+    /// <summary>
+    /// Microsoft 公司推出的关系型数据库管理系统
+    /// </summary>
+    public class MicrosoftSqlServerDatabase : egg.db.Database {
+
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 端口号
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// 数据库名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 对象实例化
+        /// </summary>
+        public MicrosoftSqlServerDatabase() {
+            base.Type = egg.db.DatabaseTypes.Microsoft_SQL_Server;
+            this.Port = 1433;
+        }
+
+        /// <summary>
+        /// 创建连接管理器
+        /// </summary>
+        /// <returns></returns>
+        protected override IConnectionable OnCreateConnection() {
+            return new MicrosoftSqlServerConnection();
+        }
+
+        /// <summary>
+        /// 获取字符串表现形式
+        /// </summary>
+        /// <returns></returns>
+        protected override string OnParseString() {
+            return "data source=" + this.Address + "," + this.Port + ";user id=" + this.User + ";Password=" + this.Password + ";Initial Catalog=" + this.Name + ";Pooling=true;Max Pool Size=40000;Min Pool Size=0;";
+        }
+
+    }
+}
---
+++ egg.db/SqlUnits/Functions/Sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace egg.db.SqlUnits.Functions {
+
+    /// <summary>
+    /// 求和函数
+    /// </summary>
+    public class Sum : Basic, ISqlStringable, ISqlAsable {
+
+        // 对象
+        private ISqlStringable _object;
+
+        /// <summary>
+        /// 对象实例化
+        /// </summary>
+        /// <param name="obj"></param>
+        public Sum(ISqlStringable obj) : base("SUM") {
+            _object = obj;
+        }
+
+        /// <summary>
+        /// 获取是否为复杂对象
+        /// </summary>
+        public bool IsComplicated { get { return false; } set { } }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tp"></param>
+        /// <param name="multiTable"></param>
+        /// <returns></returns>
+        public string ToSqlString(DatabaseTypes tp, bool multiTable = false) {
+            return $"{this.Name}({_object.ToSqlString(tp, multiTable)})";
+        }
+
+        /// <summary>
+        /// 重命名
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public As As(string name) { return new As(this, name); }
+    }
+}
