87740333ba1f89cd1c546643b56a9b517e7bf5d3
inmount <inmount@live.cn>
2021-01-24 22:27:28 +0800

    变更MVC工作方式

--- egg.Mvc/ApiControllerBase.cs
+++ egg.Mvc/ApiControllerBase.cs
@@ -22,17 +22,99 @@ namespace egg.Mvc {
         /// <summary>
         /// 输出对象
         /// </summary>
-        protected egg.Jttp.JttpResponse JResponse { get; private set; }
+        public egg.Jttp.JttpResponse JResponse { get; private set; }
 
         /// <summary>
         /// 获取格式化内容后的对象
         /// </summary>
-        protected egg.Jttp.JttpRequest JRequest { get; private set; }
+        public egg.Jttp.JttpRequest JRequest { get; private set; }
 
         /// <summary>
         /// 获取表单内容对象
         /// </summary>
         protected egg.KeyValues<string> Form { get; private set; }
+
+        #region [=====集中处理字段集合=====]
+
+        private List<ApiControllerFieldSetting> _fields;
+
+        /// <summary>
+        ///  清理字段设定
+        /// </summary>
+        protected void FieldsClear() { _fields.Clear(); }
+
+        /// <summary>
+        /// 添加字段设定
+        /// </summary>
+        /// <param name="name"></param>
+        protected void AddField(string name) { _fields.Add(new ApiControllerFieldSetting() { Name = name }); }
+
+        /// <summary>
+        /// 添加字段设定
+        /// </summary>
+        /// <param name="name"></param>
+        protected void AddField(string name, bool isMust, bool isEnable = true) { _fields.Add(new ApiControllerFieldSetting() { Name = name, IsMust = isMust, Enabled = isEnable }); }
+
+        /// <summary>
+        /// 添加字段设定
+        /// </summary>
+        /// <param name="name"></param>
+        protected void AddField(string name, ApiControllerFieldSetting.DataTypes types, bool isMust = false, bool isEnable = true) { _fields.Add(new ApiControllerFieldSetting() { Name = name, DataType = types, IsMust = isMust, Enabled = isEnable }); }
+
+        // 查找字段设定
+        private ApiControllerFieldSetting FindFieldSetting(string name) {
+            for (int i = 0; i < _fields.Count; i++) {
+                if (_fields[i].Name == name) return _fields[i];
+            }
+            return null;
+        }
+
+        // 获取字段设定
+        private ApiControllerFieldSetting GetFieldSetting(string name) {
+            ApiControllerFieldSetting field = FindFieldSetting(name);
+            if (eggs.IsNull(field)) field = FindFieldSetting("*");
+            return null;
+        }
+
+        /// <summary>
+        /// 设置字段设定
+        /// </summary>
+        /// <param name="name"></param>
+        protected void SetField(string name) {
+            ApiControllerFieldSetting field = FindFieldSetting(name);
+            if (eggs.IsNull(field)) AddField(name);
+        }
+
+        /// <summary>
+        /// 设置字段设定
+        /// </summary>
+        /// <param name="name"></param>
+        protected void SetField(string name, bool isMust, bool isEnable = true) {
+            ApiControllerFieldSetting field = FindFieldSetting(name);
+            if (eggs.IsNull(field)) {
+                AddField(name, isMust, isEnable);
+            } else {
+                field.IsMust = isMust;
+                field.Enabled = isEnable;
+            }
+        }
+
+        /// <summary>
+        /// 设置字段设定
+        /// </summary>
+        /// <param name="name"></param>
+        protected void SetField(string name, ApiControllerFieldSetting.DataTypes types, bool isMust = false, bool isEnable = true) {
+            ApiControllerFieldSetting field = FindFieldSetting(name);
+            if (eggs.IsNull(field)) {
+                AddField(name, types, isMust, isEnable);
+            } else {
+                field.DataType = types;
+                field.IsMust = isEnable;
+                field.Enabled = isEnable;
+            }
+        }
+
+        #endregion
 
         /// <summary>
         /// 获取文本内容
@@ -81,6 +163,9 @@ namespace egg.Mvc {
         /// <returns></returns>
         protected string Initialize() {
 
+            // 建立Jttp应答器
+            this.JResponse = new Jttp.JttpResponse();
+
             // Json数据模式
             if (_is_request_json) {
                 byte[] buffer = new byte[4096];
@@ -98,18 +183,61 @@ namespace egg.Mvc {
 
                 // 解析获取到的数据
                 this.JRequest = (egg.Jttp.JttpRequest)eggs.ParseJson(content, typeof(egg.Jttp.JttpRequest));
+
+                // 检测所有的必要参数
+                foreach (var field in _fields) {
+                    if (field.IsMust) {
+                        if (!JRequest.Form.ContainsKey(field.Name)) {
+                            JResponse.SetFail($"缺少必要的 '{field.Name}' 参数");
+                            return JResponse.ToJson();
+                        }
+                    }
+                }
+
+                // 填充表单数据
+                this.Form = new KeyValues<string>();
+                foreach (string key in JRequest.Form.GetNames()) {
+                    var field = GetFieldSetting(key);
+                    if (!eggs.IsNull(field)) {
+                        if (field.Enabled) {
+                            switch (field.DataType) {
+                                case ApiControllerFieldSetting.DataTypes.Decimal: this.Form[key] = JRequest.Form[key].GetNumber().ToString(); break;
+                                case ApiControllerFieldSetting.DataTypes.Long: this.Form[key] = ((long)JRequest.Form[key].GetNumber()).ToString(); break;
+                                case ApiControllerFieldSetting.DataTypes.Integer: this.Form[key] = ((int)JRequest.Form[key].GetNumber()).ToString(); break;
+                                default: this.Form[key] = "" + JRequest.Form[key].GetString(); break;
+                            }
+                        }
+                    }
+                }
             }
 
             // 表单数据模式
             if (_is_request_form) {
+                // 检测所有的必要参数
+                foreach (var field in _fields) {
+                    if (field.IsMust) {
+                        if (!Request.Form.ContainsKey(field.Name)) {
+                            JResponse.SetFail($"缺少必要的 '{field.Name}' 参数");
+                            return JResponse.ToJson();
+                        }
+                    }
+                }
+                // 填充表单数据
                 this.Form = new KeyValues<string>();
                 foreach (string key in Request.Form.Keys) {
-                    this.Form[key] = Request.Form[key];
+                    var field = GetFieldSetting(key);
+                    if (!eggs.IsNull(field)) {
+                        if (field.Enabled) {
+                            switch (field.DataType) {
+                                case ApiControllerFieldSetting.DataTypes.Decimal: this.Form[key] = ((string)Request.Form[key]).ToDouble().ToString(); break;
+                                case ApiControllerFieldSetting.DataTypes.Long: this.Form[key] = ((string)Request.Form[key]).ToLong().ToString(); break;
+                                case ApiControllerFieldSetting.DataTypes.Integer: this.Form[key] = ((string)Request.Form[key]).ToInteger().ToString(); break;
+                                default: this.Form[key] = Request.Form[key]; break;
+                            }
+                        }
+                    }
                 }
             }
-
-            // 建立Jttp应答器
-            this.JResponse = new Jttp.JttpResponse();
 
             // 返回初始化重载事件
             return this.OnInit();
@@ -126,6 +254,8 @@ namespace egg.Mvc {
             if (DefaultMode == ApiControllerMode.Jttp) {
                 this.SetRequestJsonMode();
             }
+            _fields = new List<ApiControllerFieldSetting>();
+            SetField("*", ApiControllerFieldSetting.DataTypes.String, false, true);
             //this.Initialize();
         }
 
@@ -157,6 +287,45 @@ namespace egg.Mvc {
                 return Error(0, ex.Message);
             }
         }
+
+        /// <summary>
+        /// 创建上下文
+        /// </summary>
+        /// <returns></returns>
+        protected virtual ApiControllerRenderContext OnCreateRenderContext() { return new ApiControllerRenderContext(this, Form); }
+
+        /// <summary>
+        /// 呈现之前事件
+        /// </summary>
+        /// <returns></returns>
+        protected virtual bool OnBeforeRender(ApiControllerRenderContext context) { return true; }
+
+        /// <summary>
+        /// 呈现后事件
+        /// </summary>
+        /// <returns></returns>
+        protected virtual void OnAfterRender(ApiControllerRenderContext context) { }
+
+        /// <summary>
+        /// 呈现器
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        protected string Render(Action<ApiControllerRenderContext> action) {
+            try {
+                Initialize();
+                var context = this.OnCreateRenderContext();
+                if (this.OnBeforeRender(context)) {
+                    action(context);
+                }
+                this.OnAfterRender(context);
+                return context.Response.ToJson();
+            } catch (Exception ex) {
+                return Error(0, ex.Message);
+            }
+        }
+
+        #region [=====直接操作模式=====]
 
         /// <summary>
         /// 返回成功数据
@@ -224,6 +393,8 @@ namespace egg.Mvc {
             this.OnRender();
             return JResponse.ToJson();
         }
+
+        #endregion
 
     }
 }
--- egg.Mvc/ApiControllerFieldSetting.cs
+++ egg.Mvc/ApiControllerFieldSetting.cs
@@ -36,6 +36,11 @@ namespace egg.Mvc {
         }
 
         /// <summary>
+        /// 获取或设置名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
         /// 获取或设置可用状态
         /// </summary>
         public bool Enabled { get; set; }
