5bcd174e3f4e9a50244e295a8f5c53b251bf03a9
Conglin Zheng <inmount@live.cn>
2022-05-28 14:59:39 +0800

    继续整理eggs分类

--- egg/eggs/eggs.Console.cs
+++ egg/eggs/eggs.Console.cs
@@ -7,13 +7,19 @@ using egg;
 public partial class eggs {
 
     /// <summary>
-    /// 获取控制台参数集合
-    /// 参数可/或-字符进行指定
+    /// 控制台相关函数集合
     /// </summary>
-    /// <param name="args"></param>
-    /// <returns></returns>
-    public static egg.Console.Arguments GetConsoleArgs(string[] args) {
-        return new egg.Console.Arguments(args);
+    public static class Console {
+        /// <summary>
+        /// 获取控制台参数集合
+        /// 参数可/或-字符进行指定
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static egg.Console.Arguments GetConsoleArgs(string[] args) {
+            return new egg.Console.Arguments(args);
+        }
+
     }
 
 }
