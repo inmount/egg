70dd829b0ce4d3f1813dc386751579a16b570acb
Conglin Zheng <inmount@live.cn>
2022-12-22 09:34:45 +0800

    新增：【EFCore】添加更新器的概念

---
+++ Egg.EFCore/Updater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Egg.EFCore
+{
+    /// <summary>
+    /// 更新器
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Updater<T> where T : class
+    {
+        /// <summary>
+        /// 更新器
+        /// </summary>
+        public Updater()
+        {
+            var tp = typeof(T);
+        }
+    }
+}
