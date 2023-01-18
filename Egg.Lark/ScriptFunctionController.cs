756832e233a022052d8d9569d7dd2d0131361104
Conglin Zheng <inmount@live.cn>
2023-01-18 15:06:25 +0800

    修改：【Lark】兼容函数控制器

---
+++ Egg.Lark/ScriptFunctionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Egg.Lark
+{
+    /// <summary>
+    /// 函数控制器
+    /// </summary>
+    public abstract class ScriptFunctionController : IScriptFunctionController
+    {
+        /// <summary>
+        /// 获取关联引擎
+        /// </summary>
+        public ScriptEngine Engine { get; private set; }
+
+        /// <summary>
+        /// 设置脚本引擎
+        /// </summary>
+        /// <param name="engine"></param>
+        public void SetEngine(ScriptEngine engine)
+        {
+            this.Engine = engine;
+        }
+    }
+}
