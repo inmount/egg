1e7ad612521427e153fc86465cd211c974775668
Conglin Zheng <inmount@live.cn>
2023-01-19 15:35:42 +0800

    修改：【Egg】增加对可空对象和类型的助手处理

--- Egg/Extensions/ObjectExtensions.cs
+++ Egg/Extensions/ObjectExtensions.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace Egg {
+namespace Egg
+{
 
     /// <summary>
     /// 对象扩展类
     /// </summary>
-    public static class ObjectExtensions {
+    public static class ObjectExtensions
+    {
 
         /// <summary>
         /// 判断是否为空
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public static bool IsNull(this object obj) {
+        public static bool IsNull(this object? obj)
+        {
             return obj is null;
         }
 
@@ -26,8 +29,21 @@ namespace Egg {
         /// <param name="obj"></param>
         /// <param name="defaultObj"></param>
         /// <returns></returns>
-        public static T ToNotNull<T>(this object obj, T defaultObj) {
-            return (T)(obj ?? (defaultObj ?? new object()));
+        public static T ToNotNull<T>(this object? obj, T? defaultObj) where T : struct
+        {
+            return (T)(obj ?? (defaultObj ?? default(T)));
+        }
+
+        /// <summary>
+        /// 获取不为空的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="defaultObj"></param>
+        /// <returns></returns>
+        public static T ToNotNull<T>(this object? obj, T defaultObj) where T : class
+        {
+            return (T)(obj ?? (defaultObj));
         }
 
         /// <summary>
@@ -36,7 +52,8 @@ namespace Egg {
         /// <typeparam name="T"></typeparam>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public static T ToNotNull<T>(this object obj) where T : new() {
+        public static T ToNotNull<T>(this object? obj) where T : new()
+        {
             if (obj is null) return new T();
             return (T)obj;
         }
---
+++ Egg/Extensions/TypeExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Egg
+{
+
+    /// <summary>
+    /// 类型扩展类
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// 获取外层类型名称
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        public static string GetTopName(this Type? type)
+        {
+            if (type is null) return "System.Null";
+            return type.Namespace + "." + type.Name;
+        }
+
+        /// <summary>
+        /// 判断是否为数值类型
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(this Type? type)
+        {
+            if (type is null) return false;
+            if (type.BaseType != null) return type.BaseType.IsNumeric();
+            string topName = type.GetTopName();
+            switch (topName)
+            {
+                case "System.Byte":
+                case "System.SByte":
+                case "System.Decimal":
+                case "System.Single":
+                case "System.Double":
+                case "System.Int16":
+                case "System.Int32":
+                case "System.Int64":
+                case "System.UInt16":
+                case "System.UInt32":
+                case "System.UInt64":
+                case "System.Boolean":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+    }
+}
