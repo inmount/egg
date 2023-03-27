aa780ea09e72dcb65a7ec51f79b1c39315f494e4
Lin Zheng <inmount@live.cn>
2023-03-27 10:19:12 +0800

    新增：【Lark】 添加Lark项目Demo

---
+++ Demo/Egg.Lark.SimpleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string code = "return(let(a, \"Hello World\"), a)"; 
+Console.WriteLine(code);
+using (var script = Egg.Lark.ScriptParser.Parse(code))
+using (var funcs = new Egg.Lark.ScriptFunctions())
+{
+    using (var engine = new Egg.Lark.ScriptEngine(script, funcs))
+    {
+        Console.WriteLine(engine.Execute());
+    }
+}
